8cbd285cefed397e4000b6da819bbe4e18c156ea
Igor Souza <igormcsouza@gmail.com>
2019-05-29 19:28:57 -0300

    create first model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class PlusOne(models.Model):
+    yes_or_no = [('y', 'yes'), ('n', 'no')]
+    choise = models.CharField(
+        max_length=1,
+        choices=yes_or_no
+    )
+    data = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(max_length=100)
