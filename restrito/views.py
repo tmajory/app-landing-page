94cf211145a94756c7c93f517c745fe34b6599ff
Dual Junior <admfindualjr@gmail.com>
2019-06-04 16:12:40 -0300

    fixed restrito, added link for Dados

--- restrito/views.py
+++ restrito/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def restrito(request):
-    return "Under Development"
+    return HttpResponse("Under Development")
     #return render(request, 'home/index.html')
