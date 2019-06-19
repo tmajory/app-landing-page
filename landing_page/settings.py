d7bd068c336d4091477ee20fe3e652c1b1385ffe
Dual Junior <admfindualjr@gmail.com>
2019-06-19 15:38:41 -0300

    fixed bugs at dotenv

--- landing_page/settings.py
+++ landing_page/settings.py
@@ -17,15 +17,15 @@ import django_heroku
 #django_heroku.settings(locals())
 #Use Postgres/heroku
 import dj_database_url
-import dotenv
+#import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # To create a path variable:
 dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
+#if os.path.isfile(dotenv_file):
+    #dotenv.load_dotenv(dotenv_file)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
